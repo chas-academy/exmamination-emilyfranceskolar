ecbe435fbc764e02833a0fc585f1c0ec218fce6b
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2025-05-30 21:32:44 +0200

    Changes after failing

--- src/script.js
+++ src/script.js
@@ -1,133 +1,75 @@
-//get & handle all the html elements
-const desc = document.getElementById("desc");
-const amount = document.getElementById("amount");
+//get all the html elements
+const descriptionInput = document.getElementById("desc");
+const amountInput = document.getElementById("amount");
 const incomeBtn = document.getElementById("incomeBtn");
 const expenseBtn = document.getElementById("expenseBtn");
-
 const incomeList = document.getElementById("incomeList");
 const expenseList = document.getElementById("expenseList");
 const transactionList = document.getElementById("transactionList");
-const balance = document.getElementById("balance");
+const balanceTotal = document.getElementById("balance");
 
-//arrays to keep all transactions, income, expenses 
-let transaction = [];
+//arrays for income, expenses and transactions
 let income = [];
 let expense = [];
+let transactions = [];
 
 //to keep track of total balance
 let total = 0;
 
-function addIncome(){
-    const incomeDesc = desc.value;
-    const incomeAmount = Number(amount.value);
-    
-    const newTransaction = {
-        desc: incomeDesc,
-        amount: incomeAmount,
-        type: "income" || "expense",
-    };
-    
-    total += incomeAmount;
-    //console.log(total);
-    let isIncome = false;
+//function to add an entry with type as parameter
+function addEntry(type){
 
-    for (let item of income){
-        if (item.desc === incomeDesc){
-            item.type = "income";
-            isIncome = true;
-        }
-    } if (!isIncome){
-        //console.log(income);
-        //console.log(transaction);
-        income.push(newTransaction);
-        transaction.push(newTransaction);
+    //get user input for desc & amount
+    const description = descriptionInput.value
+    const amount = Number(amountInput.value);  
+
+    //new object to place all new entries
+    const newEntry = {description, amount, type};
+
+    // if newEntry's type === "income", then <add> newEntry's amount to the total, add all of newEntry to expense, transaction []s w/ push method
+    // then call the show output function with the lists/arrays for income and transaction and object newEntry as parameters
+    if (type === "income"){
+        total += newEntry.amount;
+        income.push(newEntry);
+        transactions.push(newEntry);
+        showOutput(incomeList, newEntry);
+        showOutput(transactionList, newEntry);
+    } else {
+    //otherwise subtract object's amount from total, add to expense and transaction []s w/ push method, then call the show output function as above
+        total -= newEntry.amount;
+        expense.push(newEntry);
+        transactions.push(newEntry);
+        showOutput(expenseList, newEntry);
+        showOutput(transactionList, newEntry);
     }
-    //clear earlier entries
-    desc.value = "";
-    amount.value = "";
-    //call the show income function
-    showIncome();
+    //call function to clear input
+     clearInput();
 }
 
-function addExpense(){
-    const incomeDesc = desc.value;
-    const incomeAmount = Number(amount.value);
-    
-    const newTransaction = {
-        desc: incomeDesc,
-        amount: incomeAmount,
-        type: "income" || "expense",
-    };
-    
-    let isExpense = false;
-    total -= incomeAmount;
-    //console.log(total);
-    
-    for (let item of expense){
-        if (item.desc === incomeDesc){
-            item.type = "expense";
-            isExpense = true;
-        }
-    } if (!isExpense){
-        //console.log(expense);
-        //console.log(transaction);
-        expense.push(newTransaction);
-        transaction.push(newTransaction);
-    }
-    //clear earlier entries
-    desc.value = "";
-    amount.value = "";
-    //call the show the expense function
-    showExpense();
+//added function to clear input and reduce previous redundancy
+function clearInput(){
+    descriptionInput.value = "";
+    amountInput.value = "";
 }
-//function to print out all the income as a list
-function showIncome(){
-    //clear earlier entries
-    incomeList.innerHTML = ""; 
-    for (let item of income){
-        const li = document.createElement("li");
-        li.textContent = `${item.desc}: ${item.amount}kr`;
-        incomeList.appendChild(li);
-    }
-}
-//function to print out all the expenses as a list
-function showExpense(){
-    //clear earlier entries
-    expenseList.innerHTML = ""; 
-    for (let item of expense){
-        const li = document.createElement("li");
-        li.textContent = `${item.desc}: ${item.amount}kr`;
-        expenseList.appendChild(li);
-    }
-}
-//function to show all transactions
-function showTransaction(){
-    //clear earlier entries
-    transactionList.innerHTML = ""; 
-    for (let item of transaction){
-        const li = document.createElement("li");
-        li.textContent = `${item.desc}: ${item.amount}kr`;
-        transactionList.appendChild(li);
-    }
-}
-//show the total balance kept in the variable total
-function showBalance(){
-    balance.innerText = "";
+//function to show output with a list & object as parameters
+function showOutput(someList, newEntry){
+
+    //clear so not all entries show, just the final end total
+    balanceTotal.innerHTML = "";
+
+    //create list element & output for all lists, before I had a lot of redundant code 
+    const li = document.createElement("li");                                 
+    li.textContent = `${newEntry.description} - ${newEntry.amount}kr (${newEntry.type === "income" ? "Inkomst" : "Utgift"})`; // show inkomst or utgift depending on the objects's type
+    someList.appendChild(li);
+
     const text = document.createElement("p");
-    text.textContent = "Saldo: " + total + " kr";
-    balance.appendChild(text);
+    text.textContent = total;
+    balanceTotal.appendChild(text);
 }
 
 incomeBtn.addEventListener("click", () => {
-    addIncome();
-    showTransaction();
-    showBalance();
+    addEntry("income");
 })
-
 expenseBtn.addEventListener("click", () => {
-    addExpense();
-    showTransaction();
-    showBalance();
+    addEntry("expense");
 })
-
-
