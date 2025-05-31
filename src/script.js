391ee61a5d4142aba0926de8887be68a5edae35b
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2025-05-31 20:43:35 +0200

    Works in my browser, not sure why it doesn't work in the tests..

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ const descriptionInput = document.getElementById("desc");
 const amountInput = document.getElementById("amount");
 const incomeBtn = document.getElementById("incomeBtn");
 const expenseBtn = document.getElementById("expenseBtn");
+
 const incomeList = document.getElementById("incomeList");
 const expenseList = document.getElementById("expenseList");
 const transactionList = document.getElementById("transactionList");
@@ -11,7 +12,7 @@ const balance = document.getElementById("balance");
 //arrays for income, expenses and transactions
 let income = [];
 let expense = [];
-let transactions = [];
+let transaction = [];
 
 //to keep track of total balance
 let total = 0;
@@ -28,17 +29,17 @@ function addEntry(type){
 
     // if newEntry's type === "income", then <add> newEntry's amount to the total, add all of newEntry to expense, transaction []s w/ push method
     // then call the show output function with the lists/arrays for income and transaction and object newEntry as parameters
-    if (type === "income"){
+    if (newEntry.type === "income"){
         total += newEntry.amount;
         income.push(newEntry);
-        transactions.push(newEntry);
+        transaction.push(newEntry);
         showOutput(incomeList, newEntry);
         showOutput(transactionList, newEntry);
     } else {
     //otherwise subtract object's amount from total, add to expense and transaction []s w/ push method, then call the show output function as above
         total -= newEntry.amount;
         expense.push(newEntry);
-        transactions.push(newEntry);
+        transaction.push(newEntry);
         showOutput(expenseList, newEntry);
         showOutput(transactionList, newEntry);
     }
@@ -52,7 +53,7 @@ function clearInput(){
     amountInput.value = "";
 }
 //function to show output with a list & object as parameters
-function showOutput(someList, newEntry){
+function showOutput(aList, newEntry){
 
     //clear so not all entries show, just the final end total
     balance.innerHTML = "";
@@ -60,7 +61,7 @@ function showOutput(someList, newEntry){
     //create list element & output for all lists, before I had a lot of redundant code 
     const li = document.createElement("li");                                 
     li.textContent = `${newEntry.description} - ${newEntry.amount} kr (${newEntry.type === "income" ? "Inkomst" : "Utgift"})`; // show inkomst or utgift depending on the objects's type
-    someList.appendChild(li);
+    aList.appendChild(li);
 
     const text = document.createElement("p");
     text.textContent = total;
